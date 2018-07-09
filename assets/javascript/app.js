a9e50c1e5a338d60084969298b68187034ef560e
louisqvo <louisqvo@gmail.com>
2018-07-09 16:23:43 -0400

    players pop up on the other page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,58 +7,57 @@ var win2 = 0;
 var lost1 = 0;
 var lost2 = 0;
 var keys = [];
-var filled = 1;
+var filled;
 var name;
 
 var usersRef = database.ref().child("players");
 var chatMess = database.ref().child("chat");
+$("#player1Btn").hide();
+$("#player2Btn").hide();
 
 $("#nameSubmit").click(function(){
     name = $("#name").val();
+    console.log($("#player1").attr("data-fill"));
+    console.log($("#name").val());
+    $("#name").val("");
 
     if(name === ""){
         alert("please enter a name")
     }else{
-
-        usersRef.once('value', function(snapshot) {
-            if (snapshot.hasChild("1")) {
-                usersRef.child("2").set({
-                    name: name,
-                    win: win1,
-                    lost: lost1
-                });
-            }else{
-                usersRef.child("1").set({
-                    name: name,
-                    win: win1,
-                    lost: lost1
-                });
-            }
-          }).then(function(){
-            gameBtn(name, filled);
-        });
+        if($("#player1").attr("data-fill") === "unfill"){
+            usersRef.child("1").set({
+                name: name,
+                win: win1,
+                lost: lost1
+            });
+            console.log(name + "2");
+            $("#text1").text(name);
+            $("#player1Btn").show();
+        }else {
+            usersRef.child("2").set({
+                name: name,
+                win: win1,
+                lost: lost1
+            });
+            $("#text2").text(name);
+            $("#player2Btn").show();
+        }
     }
     $(".nameInput").hide();
 });
 
-
 database.ref().child("players").on("child_added", function(snap) {
-    console.log($("#player1").attr("data-fill"));
-    console.log(snap.val())
 
-
-    if($("#player1").attr("data-fill") === "filled"){
-        $("#text2").text(snap.val()[2].name);
-    }else{
-        $("#text1").text(snap.val()[1].name);
+    if($("#player1").attr("data-fill") === "unfill"){
+        $("#text1").text(name);
+    }else {
+        $("#text2").text(name);
     }
+    $("#player1").attr("data-fill", "fill");
 });
 
-
-
-
 $(document).on("click","button.btn1", function(){
-    console.log("fff");
+    console.log($(this).val());
 })
 
 $(document).on("click","button.btn2", function(){
@@ -68,6 +67,8 @@ $(document).on("click","button.btn2", function(){
 $("#textSubmit").click(function(){
 
     chatMess.push($("#inputMessage").val());
+
+    $("#inputMessage").val("");
 
 });
 
@@ -80,27 +81,18 @@ database.ref().child("chat").on("child_added", function(snap) {
     $("#messages").append(mess);
 });
 
+function myFunction1(event) {
 
-function gameBtn(name , odd){
+    var x = event.key;
+    if(x === "Enter"){
+        $("#nameSubmit").click();
+    }
+}
 
-    $("#text" + odd).text(name);
+function myFunction(event) {
 
-    var rock = $("<button>");
-    var paper = $("<button>");
-    var scissor = $("<button>");
-
-    rock.text("rock");
-    paper.text("paper");
-    scissor.text("scissor");
-    rock.addClass("btn" + odd);
-    paper.addClass("btn" + odd);
-    scissor.addClass("btn" + odd);
-    rock.attr("id", "rock" + odd);
-    paper.attr("id", "paper" + odd);
-    scissor.attr("id", "scissor" + odd);
-
-    $("#player" + odd).append(rock);
-    $("#player" + odd).append(paper);
-    $("#player" + odd).append(scissor);
-
+    var x = event.key;
+    if(x === "Enter"){
+        $("#textSubmit").click();
+    }
 }
